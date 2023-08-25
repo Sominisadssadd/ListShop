0185320cf9df2320da3ffd8674da467ceb26f67d
Yaroslav <zinakola2004@gmail.com>
2023-08-25 21:03:19 +0500

    Add ShopItemActivity in AddShopItemActivity branch

---
+++ app/src/main/java/com/example/listshop/presentation/ShopItemActivity.kt
@@ -0,0 +1,14 @@
+package com.example.listshop.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.listshop.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+
+
+    }
+}
